f6e2bb885e16520e2c9b662a5dcf38f3f8bb2867
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2024-09-14 20:08:22 +0530

    g

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,6 +13,7 @@ const UserSchema = new mongoose.Schema(
         }
 }, { timestamps: true })
 
+
 //create collection
 const UserModel = mongoose.model('users', UserSchema)
 module.exports = UserModel
