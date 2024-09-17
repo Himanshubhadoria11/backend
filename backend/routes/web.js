276a2d1bd961175e7c4827841456dab1cdc9becf
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2024-09-17 20:14:01 +0530

    q

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const UserController = require('../controllers/UserController')
+const UserController = require('../controller/UserController')
 const route = express.Router()
 
 //usercontroller
